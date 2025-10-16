868434229e8ffd30f44b79fc47cd712bf0d7af45
ValerioVenceslai-synthara <valeriovenceslai@synthara.ai>
2025-10-16 20:01:50 +0200

    Moved and solved issue in perf analyzer

--- riscv-uvm-model/perf_analyzer/perf_analyzer.py
+++ riscv-uvm-model/perf_analyzer/perf_analyzer.py
@@ -3,6 +3,7 @@ import subprocess
 from datetime import datetime
 import getpass
 import argparse
+import yaml
 
 """
 perf_analyzer.py
@@ -40,10 +41,10 @@ Notes:
     - Assumes the presence of run-vcs.py in the parent directory of the script.
 """
 
-TEST_LIST = ["uvmt_cv32e20_firmware_test_c", "uvmt_cv32e20_model_test_c"]
-PROGRAM_LIST = ["hello-world", "fibonacci", "riscv_arithmetic_basic_test_0"]
+failed_tests = []
+passed_tests = []
 
-def compile_programs(out_dir: str, programs: list = PROGRAM_LIST):
+def compile_programs(out_dir: str, programs: list = None):
     for program in programs:
         cmd = [
                 "python3", f"{run_vcs_dir}/run-vcs.py",
@@ -72,7 +73,7 @@ def sv_compile(run_vcs_dir: str, out_dir: str):
         print("\033[91mError during compilation. Exiting script.\033[0m")
         exit(1)
 
-def sv_sim(run_vcs_dir: str, tests: list = TEST_LIST, programs: list = PROGRAM_LIST, out_dir: str = ""):
+def sv_sim(run_vcs_dir: str, tests: list = None, programs: list = None, out_dir: str = ""):
     for prog in programs:
         for test in tests:
             test_idx = 0
@@ -85,13 +86,16 @@ def sv_sim(run_vcs_dir: str, tests: list = TEST_LIST, programs: list = PROGRAM_L
                 "-program", prog,
                 "-test_idx", str(test_idx),
                 "-uvm_verbosity", "UVM_NONE",
-                "asf", "+rand_stall_obi_disable"
+                "-asf", "+rand_stall_obi_disable"
             ]
             result = subprocess.run(cmd)
             status = result.returncode
             print(f"\033[92mTest {test} with program {prog} finished with exit code {status}.\033[0m")
             if status != 0:
                 print(f"\033[91mError running test {test} with program {prog}. Exiting script.\033[0m")
+                failed_tests.append(f"Test {test} with program {prog} FAILED!")
+            else:
+                passed_tests.append(f"Test {test} with program {prog} PASSED!")
 
 if __name__ == "__main__":
 
@@ -101,12 +105,24 @@ if __name__ == "__main__":
 
     script_dir = os.path.dirname(os.path.abspath(__file__))
 
-    run_vcs_dir = os.path.join(script_dir, "..")
+    run_vcs_dir = os.path.join(script_dir, "..", "..")
 
     print(f"Script directory: {script_dir}")
+
+    config_path = os.path.join(script_dir, "perf_analyzer_config.yml")
+    with open(config_path, "r") as f:
+        config = yaml.safe_load(f)
+    TEST_LIST = config.get("test_list", [])
+    PROGRAM_LIST = config.get("program_list", [])
 
     sv_compile(run_vcs_dir=run_vcs_dir, out_dir=args.out_dir)
     compile_programs(out_dir=args.out_dir, programs=PROGRAM_LIST)
     sv_sim(run_vcs_dir=run_vcs_dir, out_dir=args.out_dir, tests=TEST_LIST, programs=PROGRAM_LIST)
+
+    print("\nSummary of Test Results:")
+    for test in passed_tests:
+        print(f"\033[92m{test}\033[0m")
+    for test in failed_tests:
+        print(f"\033[91m{test}\033[0m")
 
     # extract_results()
